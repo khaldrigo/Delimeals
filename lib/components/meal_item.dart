2ba28c08d91d0ed023ec37d297670e51640d5909
Rodrigo Lima <86531099+khaldrigo@users.noreply.github.com>
2021-07-20 06:08:26 -0300

    Update meal_item.dart

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -8,10 +8,18 @@ class MealItem extends StatelessWidget {
   const MealItem({Key? key, required this.meal}) : super(key: key);
 
   void _selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed(
+    Navigator.of(context)
+        .pushNamed(
       AppRoutes.MEAL_DETAIL,
       arguments: meal,
-    );
+    )
+        .then((result) {
+      if (result == null) {
+        print('Sem Resultado!');
+      } else {
+        print('O nome da refeição é $result');
+      }
+    });
   }
 
   @override
