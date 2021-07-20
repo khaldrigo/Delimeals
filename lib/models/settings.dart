c676b589da7905a696a6fbb14a653e05da0e76fa
Rodrigo Lima <86531099+khaldrigo@users.noreply.github.com>
2021-07-20 06:07:03 -0300

    configurações de filtros

---
+++ lib/models/settings.dart
@@ -0,0 +1,13 @@
+class Settings {
+  bool isGlutenFree;
+  bool isLactoseFree;
+  bool isVegan;
+  bool isVegeterian;
+
+  Settings({
+    this.isGlutenFree = false,
+    this.isLactoseFree = false,
+    this.isVegan = false,
+    this.isVegeterian = false,
+  });
+}
