7c7a4740a9ff7fe926edce0fb2e2ba4460d3195a
Rodrigo Lima <86531099+khaldrigo@users.noreply.github.com>
2021-07-20 06:06:27 -0300

    drawer
    criação de drawer

---
+++ lib/components/main_drawer.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import '../utils/app_routes.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget _createItem(IconData icon, String label, VoidCallback onTap) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        label,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: onTap,
+    );
+  }
+
+  const MainDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            color: Theme.of(context).accentColor,
+            alignment: Alignment.bottomRight,
+            child: Text(
+              'Delimeals',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          _createItem(Icons.restaurant, 'Refeições',
+              () => Navigator.of(context).pushReplacementNamed(AppRoutes.HOME)),
+          _createItem(
+              Icons.settings,
+              'Configurações',
+              () => Navigator.of(context)
+                  .pushReplacementNamed(AppRoutes.SETTINGS)),
+        ],
+      ),
+    );
+  }
+}
